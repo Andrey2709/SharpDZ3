8c7204dac68d0151c20e05e354c95fad0d22ab73
Andrey2709 <57500032+Andrey2709@users.noreply.github.com>
2023-05-02 14:15:43 +0300

    first3

--- Program.cs
+++ Program.cs
@@ -27,6 +27,19 @@ a(23432);
 
 // Задача 21
 
+double g(int x, int y,int z,int r,int g,int k){
+  double lenght =  Math.Sqrt(Math.Pow((x-r),2)+Math.Pow((y-g),2)+Math.Pow((z-k),2));
+Console.WriteLine(lenght);
+    return lenght;
+
+}
+
+
+g(3,6,8,2,1,-7);
+g(7,-5,0,1,-1,9);
+
+
+
 // Задача 23
 Array d(int n){
     int [] p = new int[n];
